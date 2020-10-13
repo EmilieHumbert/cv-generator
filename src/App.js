6c55c25c9e1c58b866c1e2a84da1decdf089594f
emilie <emi.m.humbert@googlemail.com>
2020-10-13 17:35:47 +0200

    Create WorkInfo component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import GeneralInfo from "./components/GeneralInfo";
 import EducationInfo from "./components/EducationInfo";
+import WorkInfo from './components/WorkInfo'
 
 function App() {
   return (
     <div>
       <GeneralInfo />
       <EducationInfo />
+      <WorkInfo />
     </div>
   );
 }
